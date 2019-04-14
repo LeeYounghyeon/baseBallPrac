a32595f0d628edc76cf77c7fe876aac89ed47440
이영현 <leey9494@icloud.com>
2019-04-15 01:06:18 +0900

    오타 수정

--- src/com/company/Ball.java
+++ src/com/company/Ball.java
@@ -4,13 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ball {
-    List<Integer> ballList;
+    private final List<Integer> ballList;
 
     Ball(List<Integer> ballList) {
         this.ballList = ballList;
-    }
-
-    public final List<Integer> getBallList() {
-        return ballList;
     }
 }
